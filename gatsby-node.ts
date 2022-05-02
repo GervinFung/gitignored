857c2e95448466bb0288073e92115f44e75fa1cc
GervinFung <gervinfungdaxuen@gmail.com>
2022-05-03 00:40:37 +0800

    fix(gatsby-node): removed post build command

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,11 +1,7 @@
 import * as child from 'child_process';
-// Log out information after a build is done
-const onPostBuild = ({ reporter }) => {
-    reporter.info('Build has been completed');
-};
 
 const onPreBuild = () => {
     child.execSync('make build');
 };
 
-export { onPostBuild, onPreBuild };
+export { onPreBuild };
