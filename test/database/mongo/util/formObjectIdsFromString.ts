1b9042862d40b44762248d384f47b2aa1c941b64
GervinFung <gervinfungdaxuen@gmail.com>
2022-08-30 01:45:33 +0800

    feat(parse-object-id): filter out invalid object id

--- test/database/mongo/util/formObjectIdsFromString.ts
+++ test/database/mongo/util/formObjectIdsFromString.ts
@@ -4,18 +4,22 @@ import { formObjectIdsFromString } from '../../../../src/database/mongo/util';
 
 const testFormObjectIdsFromString = () =>
     describe('Form ObjectIds from String', () => {
-        it('should form id from string without delimiter at both end of id', async () => {
+        it('should form id from string without delimiter at both end of id', () => {
             const id = '507f1f77bcf86cd799439011';
             expect(formObjectIdsFromString(` ${id} `)).toStrictEqual([
                 new ObjectId(id),
             ]);
         });
-        it('should form id from string with delimiter and space at both end of each id', async () => {
+        it('should form id from string with delimiter and space at both end of each id', () => {
             const idOne = '507f1f77bcf86cd799439011';
             const idTwo = '542c2b97bac0595474108b48';
             expect(
                 formObjectIdsFromString(`${idOne}${arrayDelimiter} ${idTwo} `)
             ).toStrictEqual([new ObjectId(idOne), new ObjectId(idTwo)]);
+        });
+        it('should filter out invalid id', () => {
+            const id = 'invalid';
+            expect(formObjectIdsFromString(id)).toStrictEqual([]);
         });
     });
 
