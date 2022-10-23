51b47dd2bd636904f6660bd3115a35b0fad2cc03
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2022-10-24 01:29:23 +0800

    chore(package): update & added `cases-of-test` (#31)

--- test/api/database/mongo/query/index.ts
+++ test/api/database/mongo/query/index.ts
@@ -1,10 +1,12 @@
 import testGetContentAndName from './getContentAndName';
 import testGetLatestCommitTime from './getLatestCommitTime';
+import testCases from 'cases-of-test';
 
 const testQuery = () => {
     describe('Query', () => {
-        testGetContentAndName();
-        testGetLatestCommitTime();
+        testCases({
+            tests: [[testGetContentAndName], [testGetLatestCommitTime]],
+        });
     });
 };
 
