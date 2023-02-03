bb9b6112ac75bc0a8f2abec7c19b835be230be5a
GervinFung <gervinfungdaxuen@gmail.com>
2023-02-03 12:44:38 +0800

    fix(server): check for utf8 characters

--- test/util/index.ts
+++ test/util/index.ts
@@ -47,6 +47,9 @@ class Server {
         server.stderr?.setEncoding('utf-8');
         await new Promise<void>((resolve) => {
             server.stdout?.on('data', (data: string) => {
+                console.log({
+                    data,
+                });
                 if (data.includes('ready - started server')) {
                     resolve();
                 }
